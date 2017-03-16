806bcc62ac092b4f9b74ad7883b682af4d84e34a
Paolo Castagna <paolo@cloudera.com>
2017-03-16 07:54:41 +0000

    Some more progress...

---
+++ src/test/java/dev/TestAvroMessageFormatter.java
@@ -0,0 +1,95 @@
+package dev;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.io.EncoderFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestAvroMessageFormatter  {
+
+    private final String schema = "{" +
+    		"\"type\": \"record\", " +
+    		"\"name\": \"my_schema\", " +
+    		"\"fields\": [" +
+    		"{\"name\": \"s\", \"type\": \"string\"}, " +
+    		"{\"name\": \"b\",  \"type\": \"bytes\"}" +
+    		"]}"; 
+
+    private static final String s = "this is a string";
+    private static final byte[] b = new String("this are bytes").getBytes();
+    private static final Object o = b;
+    
+    private Schema.Parser parser;
+    private Schema avroSchema;
+    private ByteArrayOutputStream out;
+    private Encoder encoder;
+	private DatumWriter<GenericRecord> writer;
+    
+    @Before
+    public void setup() {
+    	parser =  new Schema.Parser();
+    	avroSchema = parser.parse(schema);
+		out = new ByteArrayOutputStream();
+		encoder = EncoderFactory.get().binaryEncoder(out, null);
+		writer = new GenericDatumWriter<>(avroSchema);
+    }
+    
+	@Test
+	public void testClassCastException_01() throws Exception {
+		boolean thrown = false;
+		try {
+			GenericRecord datum = new GenericData.Record(avroSchema);
+			datum.put("s", s);
+			datum.put("b", b);
+			writer.write(datum, encoder);
+		} catch (ClassCastException e) {
+			thrown = true;
+		}
+		assertTrue(thrown);
+	}
+	
+	@Test
+	public void testClassCastException_02() throws Exception {
+		boolean thrown = false;
+		try {
+			GenericRecord datum = new GenericData.Record(avroSchema);
+			datum.put("s", s);
+			datum.put("b", o);
+			writer.write(datum, encoder);
+		} catch (ClassCastException e) {
+			thrown = true;
+		}
+		assertTrue(thrown);
+	}
+
+	@Test
+	public void testClassCastException_03() throws Exception {
+		GenericRecord datum = new GenericData.Record(avroSchema);
+		datum.put("s", s);
+		datum.put("b", ByteBuffer.wrap(b));
+		writer.write(datum, encoder);
+	}
+	
+	@Test
+	public void testClassCastException_04() throws Exception {
+		GenericRecord datum = new GenericData.Record(avroSchema);
+		datum.put("s", s);
+		if ( o instanceof byte[] ) {
+			datum.put("b", ByteBuffer.wrap(((byte[]) o)));
+		} else {
+			datum.put("b", o);			
+		}
+		writer.write(datum, encoder);
+	}
+
+}
---
+++ src/test/java/dev/TestCassandraJavaAPIs.java
@@ -0,0 +1,166 @@
+package dev;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.KeyspaceMetadata;
+import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.TableMetadata;
+import com.datastax.driver.core.querybuilder.Insert;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.datastax.driver.core.querybuilder.Update;
+
+// This is for me... learning to use the Cassandra Java APIs...
+
+public class TestCassandraJavaAPIs {
+	
+	private static Cluster cluster = null;
+	private static Session session = null;
+	private static String keyspaceName = null;
+	private static String tableName = null;
+
+    @BeforeClass
+    public static void setup() {
+	    cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+	    session = cluster.connect();
+	    keyspaceName = "test_" + new Date().getTime();
+	    tableName = "table_" + new Date().getTime();
+    }
+    
+    @AfterClass
+    public static void teardown() {
+    	if (keyspaceExists(keyspaceName)) {
+    		keyspaceDelete(keyspaceName);
+    	}
+    }
+
+    @Test
+    public void testTableExistanceCreationDeletion() {
+    	assertFalse(keyspaceExists(keyspaceName));
+    	assertFalse(tableExists(keyspaceName, tableName));
+
+    	keyspaceCreate(keyspaceName);
+    	assertTrue(keyspaceExists(keyspaceName));
+
+    	assertFalse(tableExists(keyspaceName, tableName));
+    	tableCreate(keyspaceName, tableName);
+    	assertTrue(tableExists(keyspaceName, tableName));
+
+    	tableDelete(keyspaceName, tableName);
+    	assertFalse(tableExists(keyspaceName, tableName));
+
+    	keyspaceDelete(keyspaceName);
+    	assertFalse(keyspaceExists(keyspaceName));    
+    }
+    
+    @Test
+    public void testQueryBuilder() {
+    	try {
+        	keyspaceCreate(keyspaceName);
+        	tableCreate(keyspaceName, tableName);
+
+        	// Perform an insert
+        	Insert insert = QueryBuilder.insertInto(keyspaceName, tableName);
+        	insert.value("c1", 1);
+        	insert.value("c2", "One");
+        	session.execute(insert);
+        	
+        	// Perform a select
+        	Statement stmt = QueryBuilder.select().all().from(keyspaceName, tableName);
+            ResultSet rs = session.execute(stmt);
+            
+            int i = 0;
+            while ( !rs.isExhausted() ) {
+                Row row = rs.one();
+                assertTrue(row.getInt(0) == 1);
+                assertTrue(row.getString(1).equals("One"));
+                ++i;
+            }
+            assertEquals(1, i);
+            
+        	// Perform an update
+        	Statement stmtUpdate = QueryBuilder.update(keyspaceName, tableName)
+        			.with(QueryBuilder.set("c2", "Uno"))
+        			.where(QueryBuilder.eq("c1", 1));
+        	session.execute(stmtUpdate);
+        	
+        	// Perform a select and check the update
+        	stmt = QueryBuilder.select().all().from(keyspaceName, tableName);
+            rs = session.execute(stmt);
+            
+            i = 0;
+            while ( !rs.isExhausted() ) {
+                Row row = rs.one();
+                assertTrue(row.getInt(0) == 1);
+                assertTrue(row.getString(1).equals("Uno"));
+                ++i;
+            }
+            assertEquals(1, i);
+            
+            // Perform a delete
+
+        	Statement stmtDelete = QueryBuilder.delete()
+        			.from(keyspaceName, tableName)
+        			.where(QueryBuilder.eq("c1", 1));
+        	session.execute(stmtDelete);
+        	
+            i = 0;
+            while ( !rs.isExhausted() ) {
+                Row row = rs.one();
+                ++i;
+            }
+            assertEquals(0, i);
+    	} finally {
+    		keyspaceDelete(keyspaceName);
+    	}    	
+    }
+
+    private static boolean keyspaceExists (String keyspaceName) {
+    	Metadata metadata = cluster.getMetadata();
+    	List<KeyspaceMetadata> keyspacesMetadata = metadata.getKeyspaces();
+    	for (KeyspaceMetadata keyspaceMetadata : keyspacesMetadata) {
+			if ( keyspaceMetadata.getName().equals(keyspaceName) ) return true;
+		}
+    	return false;
+    }
+
+    private static boolean tableExists (String keyspaceName, String tableName) {
+    	KeyspaceMetadata keyspaceMetadata = cluster.getMetadata().getKeyspace(keyspaceName);
+    	if (keyspaceMetadata != null) {
+        	TableMetadata tableMetadata = keyspaceMetadata.getTable(tableName);
+        	return tableMetadata != null;    		
+    	} else {
+    		return false;
+    	}
+    }
+
+    private static void keyspaceCreate (String keyspaceName) {
+    	session.execute("CREATE KEYSPACE " + keyspaceName + " WITH replication = {'class':'SimpleStrategy','replication_factor':1}");
+    }
+
+    private static void keyspaceDelete (String keyspaceName) {
+    	session.execute("DROP KEYSPACE " + keyspaceName);
+    }
+
+    private static void tableCreate (String keyspaceName, String tableName) {
+    	session.execute("CREATE TABLE " + keyspaceName + "." + tableName + "( c1 int PRIMARY KEY, c2 text )");
+    }
+
+    private static void tableDelete (String keyspaceName, String tableName) {
+    	session.execute("DROP TABLE " + keyspaceName + "." + tableName);
+    }
+
+}
